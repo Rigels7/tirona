edbda122a126c50067d1047d1bda091b23ebc266
Rigels7 <hita.rigels@gmail.com>
2020-12-11 17:53:53 +0100

    se arjoni esht kd

--- routes/tirona.js
+++ routes/tirona.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.send({status:200,message:"cristianoronaldo"});
+  res.send({status:200,message:"arjonotr"});
 });
 router.get('/otr', function(req, res, next) {
     res.send({status:200,message:"boom otr"})
